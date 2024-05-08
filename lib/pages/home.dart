c32e4a929181e9565c4fbe05200082ab8f2917f3
AntoineVRG <antoine.vergez@epitech.eu>
2024-05-08 02:58:05 +0200

    [ADD] Vibration and button

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:user_management/pages/profile.dart';
+import 'package:vibration/vibration.dart';
 
 /// Page d'accueil
 class MyHomePage extends StatefulWidget {
   /// Constructeur de la d'accueil
   const MyHomePage({required this.email, super.key});
-  /// email pour donner au component sign out firebase
+
+  /// Email pour donner au component sign out firebase
   final String email;
 
   /// Titre de la page
@@ -24,12 +26,17 @@ class _MyHomePageState extends State<MyHomePage> {
         actions: <Widget>[
           IconButton(
             onPressed: () async {
-              await Navigator.push(context, MaterialPageRoute<void>(builder: (BuildContext context)=> MyProfilePage(email: widget.email,)));
+              await Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                      builder: (BuildContext context) => MyProfilePage(
+                            email: widget.email,
+                          )));
             },
             icon: Icon(
               Icons.person,
               color: Theme.of(context).colorScheme.secondary,
-          ),
+            ),
             iconSize: 30,
           ),
         ],
@@ -42,11 +49,33 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       body: Center(
-        child: Text(
-          style: TextStyle(
-            color: Theme.of(context).colorScheme.primary, 
-          ),
-          'USER_MANAGEMENT',
+        child: Column(
+          mainAxisSize: MainAxisSize
+              .min, // Make sure the column doesn't take more space than its content
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'USER_MANAGEMENT',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+            SizedBox(height: 20), // Adds spacing between text and button
+            ElevatedButton(
+              onPressed: () async {
+                if (await Vibration.hasVibrator() ?? false) {
+                  Vibration.vibrate();
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text("This device does not have a vibrator."),
+                    ),
+                  );
+                }
+              },
+              child: Text('Vibrate Phone'),
+            ),
+          ],
         ),
       ),
     );
