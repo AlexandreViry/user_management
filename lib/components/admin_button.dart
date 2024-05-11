3fa85c72fcb1bd3410bd54a2a12c52983e002c4f
AlexandreViry <alexandre.viry@epitech.eu>
2024-05-12 00:26:10 +0200

    [ADD] web notifications

--- lib/components/admin_button.dart
+++ lib/components/admin_button.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:user_management/components/notification_service_web.dart';
 import 'package:user_management/components/user_status.dart';
-import 'package:user_management/components/notification_button.dart';
 
 class AdminButton extends StatefulWidget {
   @override
@@ -18,12 +18,13 @@ class _AdminButtonState extends State<AdminButton> {
   }
 
   Future<void> _checkAdminStatus() async {
-    User? user = FirebaseAuth.instance.currentUser;
-
-    bool isAdmin = await userStatus().checkAdminStatus(user!.uid);
-    setState(() {
-      _isAdmin = isAdmin;
-    });
+    final User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final bool isAdmin = await userStatus().checkAdminStatus(user.uid);
+      setState(() {
+        _isAdmin = isAdmin;
+      });
+    }
   }
 
   @override
@@ -33,11 +34,14 @@ class _AdminButtonState extends State<AdminButton> {
       child: ElevatedButton(
         onPressed: _isAdmin
             ? () {
-              NotificationService.pushNotification(title: 'Admin button', body: 'You are an admin, this is why you are special and can press this button');
+                NotificationService.pushNotification(
+                  title: 'Admin Button',
+                  body: 'You are an admin, this is why you are special and can press this button'
+                );
               }
             : null,
         style: ElevatedButton.styleFrom(
-          backgroundColor: _isAdmin ? Theme.of(context).scaffoldBackgroundColor : Colors.grey[400],
+          backgroundColor: _isAdmin ? Theme.of(context).colorScheme.primary : Colors.grey[400],
           foregroundColor: Colors.white,
           elevation: 10,
           textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
@@ -45,16 +49,16 @@ class _AdminButtonState extends State<AdminButton> {
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(30),
           ),
-          minimumSize: const Size(double.infinity, 50), // Full-width button
+          minimumSize: const Size(double.infinity, 50),
         ),
         child: const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Icon(Icons.notifications_active),
-              SizedBox(width: 10),
-              Text('Send notification'),
-            ],
-          ),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(Icons.notifications_active),
+            SizedBox(width: 10),
+            Text('Send Notification'),
+          ],
+        ),
       ),
     );
   }
