fd07bc44399da95a768592c06a02b3b83944dd0a
AlexandreViry <alexandre.viry@epitech.eu>
2024-05-12 22:30:30 +0200

    [FIX] notifications

--- lib/components/notification_button.dart
+++ lib/components/notification_button.dart
@@ -1,19 +1,32 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:universal_html/html.dart' as html;
 
 class NotificationService {
   static final _notification = FlutterLocalNotificationsPlugin();
 
   static init() {
     _notification.initialize(const InitializationSettings(
-      android: AndroidInitializationSettings('logo'),
-      iOS: DarwinInitializationSettings()
-    ));
+        android: AndroidInitializationSettings('logo'),
+        iOS: DarwinInitializationSettings()));
   }
 
   static pushNotification({required String title, required String body}) async {
-    var iosDetails = const DarwinNotificationDetails();
-    var androidDetails = const AndroidNotificationDetails('channel_1', 'My channel', importance: Importance.max, priority: Priority.high);
-    var notificationDetails = NotificationDetails(android: androidDetails, iOS: iosDetails);
-    _notification.show(20, title, body, notificationDetails);
+    if (html.Notification.permission == 'granted') {
+      html.Notification(title, body: body);
+    } else if (html.Notification.permission != 'denied') {
+      html.Notification.requestPermission().then((permission) {
+        if (permission == 'granted') {
+          html.Notification(title, body: body);
+        }
+      });
+    } else {
+      var iosDetails = const DarwinNotificationDetails();
+      var androidDetails = const AndroidNotificationDetails(
+          'channel_1', 'My channel',
+          importance: Importance.max, priority: Priority.high);
+      var notificationDetails =
+          NotificationDetails(android: androidDetails, iOS: iosDetails);
+      _notification.show(20, title, body, notificationDetails);
+    }
   }
 }
